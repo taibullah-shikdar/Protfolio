7039b216e161c464443b6c291bcc19e9ce766ee1
Md. Taibullah Shikdar <taibullahshikdar8@gmail.com>
2025-01-09 22:43:33 +0600

    script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function myMenuFunction(){
 
 /* ----- TYPING EFFECT ----- */
  var typingEffect = new Typed(".typedText",{
-    strings : ["Designer","Youtuber","Developer"],
+    strings : ["Programmer","Developer"],
     loop : true,
     typeSpeed : 100, 
     backSpeed : 80,
